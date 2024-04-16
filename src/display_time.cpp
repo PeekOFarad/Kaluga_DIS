0d87af9520742e89a62a8be51a20550d33eb01d0
Artem Gumenyuk <xgumen00@vutbr.cz>
2024-04-16 13:44:12 +0200

    trying to fix clock

--- src/display_time.cpp
+++ src/display_time.cpp
@@ -6,11 +6,48 @@
 #include <../lib/display_time.h>
 #include <ESP32Time.h>
 
+byte omm = 99, oss = 99;
+byte xcolon = 0, xsecs = 0;
+
 void display_time(TFT_eSPI & tft, ESP32Time & esp_time){
-    tft.setTextSize(4);
-    tft.setTextColor(TFT_WHITE);
-    tft.setCursor(40, 5);
-    // char str[50];
-    // std:sprintf(str, "%d : %d : %d", t.);
-    tft.println(esp_time.getTime());
+    
+    // Update digital time
+    int xpos = 0;
+    int ypos = 85; // Top left corner ot clock text, about half way down
+    int ysecs = ypos + 24;
+
+    int hh = esp_time.getHour();
+    int mm = esp_time.getMinute();
+    int ss = esp_time.getSecond();
+
+    if (omm != mm) { // Redraw hours and minutes time every minute
+      omm = mm;
+      // Draw hours and minutes
+      if (hh < 10) xpos += tft.drawChar('0', xpos, ypos, 8); // Add hours leading zero for 24 hr clock
+      xpos += tft.drawNumber(hh, xpos, ypos, 8);             // Draw hours
+      xcolon = xpos; // Save colon coord for later to flash on/off later
+      xpos += tft.drawChar(':', xpos, ypos - 8, 8);
+      if (mm < 10) xpos += tft.drawChar('0', xpos, ypos, 8); // Add minutes leading zero
+      xpos += tft.drawNumber(mm, xpos, ypos, 8);             // Draw minutes
+      xsecs = xpos; // Sae seconds 'x' position for later display updates
+    }
+    if (oss != ss) { // Redraw seconds time every second
+      oss = ss;
+      xpos = xsecs;
+
+      if (ss % 2) { // Flash the colons on/off
+        tft.setTextColor(0x39C4, TFT_BLACK);        // Set colour to grey to dim colon
+        tft.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 6); // Seconds colon
+        tft.setTextColor(TFT_YELLOW, TFT_BLACK);    // Set colour back to yellow
+      }
+      else {
+        tft.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+        xpos += tft.drawChar(':', xsecs, ysecs, 6); // Seconds colon
+      }
+
+      //Draw seconds
+      if (ss < 10) xpos += tft.drawChar('0', xpos, ysecs, 6); // Add leading zero
+      tft.drawNumber(ss, xpos, ysecs, 6);                     // Draw seconds
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ void setup()   {
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
   tft.setTextSize(4);
-  tft.setTextColor(TFT_WHITE);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setCursor(0, 0);
   rtc.setTimeStruct(timeinfo);
   rtc.setTime(12, 12, 12, 0, 0, 0);
@@ -82,131 +82,7 @@ void loop() {
     sec--;
   }
 
+  rtc.setTime(sec, min, hour, 0, 0, 0);
+
   delay(100);
 }
-
-// #define GRIDX 120
-// #define GRIDY 90
-// #define CELLXY 4
-
-// // #define GRIDX 320
-// // #define GRIDY 240
-// // #define CELLXY 2
-
-// #define GEN_DELAY 0
-
-// //Current grid
-// uint8_t grid[GRIDX][GRIDY];
-
-// //The new grid for the next generation
-// uint8_t newgrid[GRIDX][GRIDY];
-
-// //Number of generations
-// #define NUMGEN 600
-
-// uint16_t genCount = 0;
-
-
-// //Draws the grid on the display
-// void drawGrid(void) {
-
-//   uint16_t color = TFT_WHITE;
-//   for (int16_t x = 1; x < GRIDX - 1; x++) {
-//     for (int16_t y = 1; y < GRIDY - 1; y++) {
-//       if ((grid[x][y]) != (newgrid[x][y])) {
-//         if (newgrid[x][y] == 1) color = 0xFFFF; //random(0xFFFF);
-//         else color = 0;
-//         tft.fillRect(CELLXY * x, CELLXY * y, CELLXY, CELLXY, color);
-//       }
-//     }
-//   }
-// }
-
-// //Initialise Grid
-// void initGrid(void) {
-//   for (int16_t x = 0; x < GRIDX; x++) {
-//     for (int16_t y = 0; y < GRIDY; y++) {
-//       newgrid[x][y] = 0;
-
-//       if (x == 0 || x == GRIDX - 1 || y == 0 || y == GRIDY - 1) {
-//         grid[x][y] = 0;
-//       }
-//       else {
-//         if (random(3) == 1)
-//           grid[x][y] = 1;
-//         else
-//           grid[x][y] = 0;
-//       }
-
-//     }
-//   }
-// }
-
-// // Check the Moore neighbourhood
-// int getNumberOfNeighbors(int x, int y) {
-//   return grid[x - 1][y] + grid[x - 1][y - 1] + grid[x][y - 1] + grid[x + 1][y - 1] + grid[x + 1][y] + grid[x + 1][y + 1] + grid[x][y + 1] + grid[x - 1][y + 1];
-// }
-
-// //Compute the CA. Basically everything related to CA starts here
-// void computeCA() {
-//   for (int16_t x = 1; x < GRIDX; x++) {
-//     for (int16_t y = 1; y < GRIDY; y++) {
-//       int neighbors = getNumberOfNeighbors(x, y);
-//       if (grid[x][y] == 1 && (neighbors == 2 || neighbors == 3 ))
-//       {
-//         newgrid[x][y] = 1;
-//       }
-//       else if (grid[x][y] == 1)  newgrid[x][y] = 0;
-//       if (grid[x][y] == 0 && (neighbors == 3))
-//       {
-//         newgrid[x][y] = 1;
-//       }
-//       else if (grid[x][y] == 0) newgrid[x][y] = 0;
-//     }
-//   }
-// }
-
-
-
-// void loop() {
-
-//   //Display a simple splash screen
-//   tft.fillScreen(TFT_BLACK);
-//   tft.setTextSize(2);
-//   tft.setTextColor(TFT_WHITE);
-//   tft.setCursor(40, 5);
-//   tft.println(F("Arduino"));
-//   tft.setCursor(35, 25);
-//   tft.println(F("Cellular"));
-//   tft.setCursor(35, 45);
-//   tft.println(F("Automata"));
-
-//   delay(1000);
-
-//   tft.fillScreen(TFT_BLACK);
-
-//   initGrid();
-
-//   genCount = NUMGEN;
-
-//   drawGrid();
-
-//   //Compute generations
-//   for (int gen = 0; gen < genCount; gen++)
-//   {
-//     computeCA();
-//     drawGrid();
-//     delay(GEN_DELAY);
-//     for (int16_t x = 1; x < GRIDX-1; x++) {
-//       for (int16_t y = 1; y < GRIDY-1; y++) {
-//         grid[x][y] = newgrid[x][y];
-//       }
-//     }
-
-//   }
-// }
-
-
-
-
-
