de3ea3b843bc81e7b39e7eb7757e86f4d94075a0
PeekOFarad <stepan.matej99@gmail.com>
2024-04-17 13:48:11 +0200

    setup function + main loop debug

--- src/button_ctrl.cpp
+++ src/button_ctrl.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+void ADC_setup(int resolution = 12, adc_attenuation_t attenuation = ADC_11db) {
+    analogReadResolution(resolution);
+    analogSetAttenuation(attenuation);
+}
+
 //checks adc if any button is pressed, returns 0 for none and 1-6 for coresponding buttons
 int checkButtons(int pinNum) {
     uint32_t buttonVal = analogReadMilliVolts(pinNum);
@@ -24,3 +29,18 @@ int checkButtons(int pinNum) {
     }
     return 0;
 }
+
+/*  
+
+    //main loop code for debugging. Shows returned pin number,
+    //measured millivolts (uncalibrated) and binary adc value
+    
+    tft.fillScreen(TFT_BLACK);
+    tft.setFreeFont(FF18);                 // Select the font
+    tft.drawString(sFF7, 160, 60, GFXFF);// Print the string name of the font
+    tft.setFreeFont(FF7);
+    tft.drawString(String(checkButtons(pinButtonsADC)), 160, 120, GFXFF);
+    tft.drawString(String(analogReadMilliVolts(pinButtonsADC)), 160, 150, GFXFF);
+    tft.drawString(String(analogRead(pinButtonsADC)), 160, 180, GFXFF);
+    delay(100);
+*/
